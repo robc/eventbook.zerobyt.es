556e9bf34cd35a21c5552b8d275a0d6986dfa23f
Rob Caporetto <rcaporetto@gmail.com>
2009-08-10 11:59:09 +1000

    Changing overlay colour to blank, adding tweaks to Fancybox config.

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-	$("div#content_text > a").fancybox();	
+	$("div#content_text > a").fancybox({
+		'padding': 5,
+		'overlayShow': true,
+		'overlayOpacity': 0.7,
+		'centerOnScroll': true,
+		
+	});	
 });
